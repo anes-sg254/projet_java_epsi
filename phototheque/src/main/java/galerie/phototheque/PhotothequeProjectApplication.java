f698792f3d5785b03c4c682b4abbf9c70c92b2b4
SEGHIR <anesseghir2001@gmail.com>
2024-12-17 16:14:06 +0100

    initialisation

---
+++ phototheque/src/main/java/galerie/phototheque/PhotothequeProjectApplication.java
@@ -0,0 +1,13 @@
+package galerie.phototheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotothequeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotothequeProjectApplication.class, args);
+	}
+
+}
---
+++ phototheque/src/test/java/galerie/phototheque/PhotothequeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package galerie.phototheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotothequeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
