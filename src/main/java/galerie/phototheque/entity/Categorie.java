49386a97e72b4bdd5b268e14d4fcba6429d7a6dd
VinceDerns <v.dernoncourt@ecoles-epsi.net>
2024-12-18 11:58:46 +0100

    ajout image/categorie

---
+++ src/main/java/galerie/phototheque/entity/Categorie.java
@@ -0,0 +1,7 @@
+package galerie.phototheque.entity;
+
+import org.springframework.context.annotation.Primary;
+
+public class Categorie {
+    private Long id_categorie;
+}
---
+++ src/main/java/galerie/phototheque/entity/Image.java
@@ -0,0 +1,60 @@
+package galerie.phototheque.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue
+    private Long id_image;
+
+    @Setter
+    @Getter
+    private String nom;
+
+    @Setter
+    @Getter
+    @ManyToOne
+    private Categorie categorie;
+
+    @Setter
+    @Getter
+    private String description;
+
+    @Setter
+    @Getter
+    private String taille;
+
+    @Setter
+    @Getter
+    private Date date_creation;
+
+    @Setter
+    @Getter
+    private Date date_modification;
+
+    @Setter
+    @Getter
+    private String elements_detecte;
+
+    @Setter
+    @Getter
+    private String flag_individu;
+
+    @Setter
+    @Getter
+    private String url_reel;
+
+    @Setter
+    @Getter
+    private String url_mini;
+
+}
